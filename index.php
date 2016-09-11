ac57ead9114af7580a47bf5104f6522ab0268b32
bazizi426 <bazizi426@gmail.com>
2016-09-11 12:37:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,244 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>Albatre</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css" media="all">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <!-- Start Navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <section class="row contactez-nous-holder">
+        <div class='container'>
+          <div class="contactez-nous">
+            <a href="#">
+              <span class="glyphicon glyphicon-envelope"></span> CONTACTEZ-NOUS
+            </a>
+          </div>
+        </div>
+      </section>
+      <div class="container">
+        <!-- <div class="contactez-nous">
+          <a href="#">
+            <span class="glyphicon glyphicon-envelope"></span> CONTACTEZ-NOUS
+          </a>
+        </div> -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">GESTION LOCATIVE</a></li>
+            <li><a href="#about">ESTIMATION DU LOYER</a></li>
+            <li><a href="#contact">INVERSTIRE POUR LOUER</a></li>
+            <li><a href="#contact">RECHERCHE SP&Eacute;CIFIQUE</a></li>
+            <li><a href="#contact">MON ESPACE CLIENT</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <!-- Start Banner -->
+    <div class="row banner">
+        <div class="col-xs-12 text-center banner-content">
+          <h1>VOUS ÊTES CHEZ VOUS &Agrave; <span class="casablanca">CASABLANCA</span></h1>
+          <h3>TROUVEZ UNE <strong><i>LOCATION</i></strong></h3>
+
+          <form class="container main-search-form" action="" method="post">
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-lg-1">
+              <a href="#" class="col-xs-12 btn btn-primary btn-achat search-button">ACHAT</a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+              <a href="#" class="col-xs-12 btn btn-warning btn-location search-button">LOCATION</a>
+            </div>
+
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+              <select class="form-control search-select" name="type-de-bien">
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+              </select>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+              <select class="form-control search-select" name="nombre-de-pieces">
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+              </select>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+              <select class="form-control search-select" name="nombre-de-chambres">
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+                <option value="option">option</option>
+              </select>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+              <a href="#" style="font-size: 12px" class="col-xs-8 btn btn-success btn-plus-de-details search-button plus-de-details"> <span class="glyphicon glyphicon-plus"></span>&nbsp;PLUS DE D&Eacute;TAILS</a>
+              <button type="button" class="col-xs-3 btn btn-warning btn-search search-button pull-right">
+                <span class="glyphicon glyphicon-search"></span>
+              </button>
+
+            </div>
+          </form>
+        </div>
+    </div>
+    <!-- End Banner -->
+    <div class="container content-wrapper text-center">
+      <section class="row">
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-1.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                  <h3>titre h3 de grande taille</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  </p>
+                </div>
+          </article>
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-2.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                <h3>titre h3 de grande taille</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                </p>
+              </div>
+          </article>
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-3.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                <h3>titre h3 de grande taille</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                </p>
+              </div>
+          </article>
+          <div class="clearfix hidden-sm"></div>
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-4.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                <h3>titre h3 de grande taille</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                </p>
+              </div>
+          </article>
+          <div class="clearfix visible-sm"></div>
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-5.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                <h3>titre h3 de grande taille</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                </p>
+              </div>
+          </article>
+          <article class="col-md-4 col-sm-6 col-xs-12">
+              <img src="images/article-5.jpg" class="img-responsive" alt="article 1" />
+              <div class="article-content">
+                <h3>titre h3 de grande taille</h3>
+                <p class="text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                </p>
+            </div>
+          </article>
+      </section>
+
+    </div><!-- /.container -->
+    <!-- Start Footer menu -->
+    <section class="footer-menu text-center">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-sm-3">
+            <img src="images/accord-icon.png" alt="Accord" />
+            <h4>1 er</h4>
+            <p>
+              Syndique de ce propriété à casablanca
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <img src="images/users-icon.png" alt="Users" />
+            <h4>1 er</h4>
+            <p>
+              Syndique de ce propriété à casablanca
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <img src="images/rent-icon.png" alt="Louer à casablanca" />
+            <h4>1 er</h4>
+            <p>
+              Syndique de ce propriété à casablanca
+            </p>
+          </div>
+          <div class="col-sm-3">
+            <img src="images/house.png" alt="Biens disponible" />
+            <h4>1 er</h4>
+            <p>
+              Syndique de ce propriété à casablanca
+            </p>
+          </div>
+        </div>
+        <div class="row footer-copyright">
+          <small>
+            &copy; 2015 ALBATRE TOUTS LES DROITS R&Eacute;S&Eacute;RV&Eacute;
+          </small>
+        </div>
+      </div>
+    </section>
+    <!-- End Footer menu -->
+
+    <!-- Start Footer -->
+    <footer class="text-center">
+      <div class="container">
+        .<div class="row links">
+          <div class="col-sm-3"><a href="#">Location appartement &agrave; Casablanca</a></div>
+          <div class="col-sm-3"><a href="#">Location villa - Maison à Casablanca</a></div>
+          <div class="col-sm-3"><a href="#">Location meublé à Casablanca</a></div>
+          <div class="col-sm-3"><a href="#">Location bureau à Casablanca</a></div>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer-->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
